baed6bd56d767f80d29586b829ece940efde4054
naanvaran2003-hash <naanvaran2003@gmail.com>
2026-03-12 10:04:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/paradise-nursery/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
